413a1ecaa0141afeca45f0c0521888357c1af45a
DanCamp111 <dan90002014@gmail.com>
2024-09-25 19:48:01 -0600

    sSe agrego el CRUD de categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index() {
+        return Categoria::all();
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'id_usuario' => 'required|exists:users,id',
+        ]);
+
+        $categoria = Categoria::create($validated);
+        return response()->json($categoria, 201);
+    }
+
+    public function show($id) {
+        $categoria = Categoria::findOrFail($id);
+        return $categoria;
+    }
+
+    public function update(Request $request, $id) {
+        $validated = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'id_usuario' => 'required|exists:users,id',
+        ]);
+
+        $categoria = Categoria::findOrFail($id);
+        $categoria->update($validated);
+
+        return response()->json($categoria, 200);
+    }
+
+    public function destroy($id) {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+
+        return response()->json(null, 204);
+    }
+}
+
